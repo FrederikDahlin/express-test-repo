198b1541bb912c62419a40a7c0925f7bb4f3aab1
FrederikDahlin <4027010@RTS-365.dk>
2018-02-16 13:11:04 +0100

    Manglende matriale

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/api/test', (req, res) => {
     res.json({ 
         text: "Hello World" 
     });
- });
+});
+
+app.get('/api/opgaven', () => {
+
+});     
+
+
+app.use(express.static('public'));
+
 
 app.listen(3000, (err) => {
     if (err) {
